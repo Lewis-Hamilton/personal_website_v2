9e424876d872d240705d6660de38b5a2bc04e5a3
unknown <hamiltonjlewis@gmail.com>
2019-04-11 21:41:25 -0500

    hosted

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,33 +1,102 @@
 <template>
-    <div>
-        <h1>Projects</h1>
-        <h1>blaa;lakjdfoaij eij</h1>
-    </div>
+  <div>
+    <b-card-group deck>
+      <b-card
+        title="Gold Rush 5K"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/gold_rush_logo.PNG?alt=media&token=f88bd6f5-a5fb-415d-b2a8-47b4a5b6b0ae"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>I Created the Gold Rush 5K website for the 2019 race.</b-card-text>
+
+        <b-button href="#" target="blank" variant="info">Learn More</b-button>
+        <b-button href="http://www.goldrush5k.com/" target="blank" variant="dark">Check it out</b-button>
+      </b-card>
+
+      <b-card
+        title="Personal Website"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+      >
+        <b-card-text>I Made the website you are currently using.</b-card-text>
+
+        <b-button href="#" target="blank" variant="info">Learn More</b-button>
+        <b-button href="#" target="blank" variant="dark">Check it out</b-button>
+      </b-card>
+
+      <b-card
+        title="3D-Printing"
+        img-src="https://picsum.photos/300/300/?image=41"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-text>This card has supporting text below as a natural lead-in to additional content.</b-card-text>
+        <div slot="footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </b-card>
+
+      <b-card
+        title="Games"
+        img-src="https://picsum.photos/300/300/?image=41"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-text>
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This card has even longer content than the first to show that equal height action.
+        </b-card-text>
+        <div slot="footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </b-card>
+
+      <b-card
+        title="Server"
+        img-src="https://picsum.photos/300/300/?image=41"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-text>
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This card has even longer content than the first to show that equal height action.
+        </b-card-text>
+        <div slot="footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </b-card>
+
+      <b-card
+        title="Monitor"
+        img-src="https://picsum.photos/300/300/?image=41"
+        img-alt="Image"
+        img-top
+      >
+        <b-card-text>
+          This is a wider card with supporting text below as a natural lead-in to additional content.
+          This card has even longer content than the first to show that equal height action.
+        </b-card-text>
+        <div slot="footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </b-card>
+    </b-card-group>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+a.btn.btn-info {
+  margin-right: 10px;
 }
 </style>
