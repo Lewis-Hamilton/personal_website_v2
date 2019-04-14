cede2183ef165f23f52ac3aed411bac8c837d7a1
unknown <hamiltonjlewis@gmail.com>
2019-04-14 15:42:05 -0500

    added pic for each project

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -3,7 +3,7 @@
     <b-card-group deck>
       <b-card
         title="Gold Rush 5K"
-        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/gold_rush_logo.PNG?alt=media&token=30fe3f6e-b05e-4e71-b11c-e54723b24765"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/gold_rush_thumbnail.jpg?alt=media&token=2cdfb074-5542-49b9-b165-c3e3327b4e09"
         img-alt="Image"
         img-top
       >
@@ -16,7 +16,7 @@
 
       <b-card
         title="Personal Website"
-        img-src="https://picsum.photos/600/300/?image=25"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/website_thumbnail.jpg?alt=media&token=9c576330-0708-49f8-82d1-c70300a81470"
         img-alt="Image"
         img-top
       >
@@ -29,7 +29,7 @@
 
       <b-card
         title="Hour Tracking App"
-        img-src="https://picsum.photos/600/300/?image=25"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/default.PNG?alt=media&token=7c8d87bd-0ba9-4acd-93da-fe4bbb1b992d"
         img-alt="Image"
         img-top
       >
@@ -42,7 +42,7 @@
 
       <b-card
         title="Engine"
-        img-src="https://picsum.photos/300/300/?image=41"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/engine_thumbnail.jpg?alt=media&token=636a4bd1-7faa-4ace-8ef9-d9bdcde0608c"
         img-alt="Image"
         img-top
       >
@@ -53,10 +53,10 @@
       </b-card>
     </b-card-group>
 
-    <b-card-group>
+    <b-card-group deck>
       <b-card
         title="Typing Game"
-        img-src="https://picsum.photos/300/300/?image=41"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/typing_thumbnail.PNG?alt=media&token=0b46fa88-d346-4ee9-bb07-b359ee101879"
         img-alt="Image"
         img-top
       >
@@ -68,7 +68,7 @@
 
       <b-card
         title="JS Snake"
-        img-src="https://picsum.photos/300/300/?image=41"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/snake_thumbnail.jpg?alt=media&token=6d45b170-6d9f-4c5a-906b-834e2f1bf94c"
         img-alt="Image"
         img-top
       >
@@ -80,7 +80,7 @@
 
       <b-card
         title="Server"
-        img-src="https://picsum.photos/300/300/?image=41"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/server_thumbnail.jpeg?alt=media&token=88b6f93d-1fc2-4da0-8919-e795c7b5f1ef"
         img-alt="Image"
         img-top
       >
@@ -92,7 +92,7 @@
 
       <b-card
         title="Monitor"
-        img-src="https://picsum.photos/300/300/?image=41"
+        img-src="https://firebasestorage.googleapis.com/v0/b/lewishamilton-2aac2.appspot.com/o/monitor_thumbnail.jpg?alt=media&token=72f81fcd-5fa6-48ba-9545-855bba03ba0d"
         img-alt="Image"
         img-top
       >
