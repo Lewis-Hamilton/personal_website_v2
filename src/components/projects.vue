dcca875a64d41df1dc916357bcb31fe6b3d9dea7
unknown <hamiltonjlewis@gmail.com>
2019-04-14 15:57:56 -0500

    added buttons

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -9,7 +9,7 @@
       >
         <b-card-text>I Created the Gold Rush 5K website for the 2019 race.</b-card-text>
         <div slot="footer">
-          <b-button href="#" target="blank" variant="info">Learn More</b-button>
+          <b-button href="#" variant="info">Learn More</b-button>
           <b-button href="http://www.goldrush5k.com/" target="blank" variant="dark">Check it out</b-button>
         </div>
       </b-card>
@@ -22,7 +22,7 @@
       >
         <b-card-text>I Made the website you are currently using.</b-card-text>
         <div slot="footer">
-          <b-button href="#" target="blank" variant="info">Learn More</b-button>
+          <b-button href="#" variant="info">Learn More</b-button>
           <b-button href="#" target="blank" variant="dark">Check it out</b-button>
         </div>
       </b-card>
@@ -35,7 +35,7 @@
       >
         <b-card-text>I am currently working on an app that tracks hours worked. It is primarily for hourly workers but it could be used to track work on anything.</b-card-text>
         <div slot="footer">
-          <b-button href="#" target="blank" variant="info">Learn More</b-button>
+          <b-button href="#" variant="info">Learn More</b-button>
           <b-button href="#" target="blank" variant="dark">Check it out</b-button>
         </div>
       </b-card>
@@ -48,7 +48,7 @@
       >
         <b-card-text>I 3D-Printed an engine. This is the longest print i've done so far at 24 hours of print time for the engine block alone.</b-card-text>
         <div slot="footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+          <b-button href="#" variant="info">Learn More</b-button>
         </div>
       </b-card>
     </b-card-group>
@@ -62,7 +62,7 @@
       >
         <b-card-text>This is a demo game I made in unity just to try out the concept. I plan to make a new version that is better.</b-card-text>
         <div slot="footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+          <b-button href="#" variant="info">Learn More</b-button>
         </div>
       </b-card>
 
@@ -74,7 +74,7 @@
       >
         <b-card-text>I made a simple snake game in JavaScript. I want to improve on this game and make it look better.</b-card-text>
         <div slot="footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+          <b-button href="#" variant="info">Learn More</b-button>
         </div>
       </b-card>
 
@@ -86,7 +86,7 @@
       >
         <b-card-text>I bought an old HP server. I want to host a plex server on it and I a have a few other ideas for later.</b-card-text>
         <div slot="footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+          <b-button href="#" variant="info">Learn More</b-button>
         </div>
       </b-card>
 
@@ -98,7 +98,7 @@
       >
         <b-card-text>I got an old monitor for free from work and the screen doesn't work. It turns off at random times so I am trying to fix it.</b-card-text>
         <div slot="footer">
-          <small class="text-muted">Last updated 3 mins ago</small>
+          <b-button href="#" variant="info">Learn More</b-button>
         </div>
       </b-card>
     </b-card-group>
